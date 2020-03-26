14f12824417c11d5c11736411b2de8029f8429b4
Christian Nilsson <nchris@gmail.com>
2020-03-26 20:56:45 +0100

    F and NUM layer changed
    Trying to have NUM layer key on home row on g key.
    Moved quote and tilde key as well to new position on right hand
    side so I don't have to reach as much when typing.
    Marking on layout which key activated layer to avoid wierd hand
    and finger positions to reach keys on layer

--- keyboards/kyria/keymaps/nchris/keymap.c
+++ keyboards/kyria/keymaps/nchris/keymap.c
@@ -17,50 +17,50 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * Default Layer: QWERTY
  *
  * ,-------------------------------------------.                                   ,-------------------------------------------.
- * | TAB    |   Q  |   W  |   E  |   R  |   T  |                                   |   Y  |   U  |   I  |   O  |   P  |  | \   |
+ * | TAB    |   Q  |   W  |   E  |   R  |   T  |                                   |   Y  |   U  |   I  |   O  |   P  |  ' "   |
  * |--------+------+------+------+------+------|                                   |------+------+------+------+------+--------|
- * | ESC    |   A  |   S  |  D   |   F  |   G  |                                   |   H  |   J  |   K  |   L  | ;  : |  ENT   |
+ * | ESC    |   A  |   S  |  D   |   F  |NUM, G|                                   |   H  |   J  |   K  |   L  | ;  : |  ENT   |
  * |--------+------+------+------+------+------+-------------.  ,------------------+------+------+------+------+------+--------|
- * | LSFT   |   Z  |   X  |   C  |   V  |   B  |NAV, [| F    |  | NUM,'|Shortcut, ]|   N  |   M  | ,  < | . >  | /  ? |  RSFT  |
+ * | LSFT   |   Z  |   X  |   C  |   V  |   B  |NAV, [| F    |  |  | \ |Shortcut, ]|   N  |   M  | ,  < | . >  | /  ? |  RSFT  |
  * `----------------------+------+------+------+------+------|  |------+-----------+------+------+------+----------------------'
  *                        |      | LGUI |      | Space|      |  | Lead | Space     |      | RGUI |      |
  *                        |      |      | LCTL |      | Alt  |  |      |           | BKSPC|      |      |
  *                        `----------------------------------'  `---------------------------------------'
  */
     [QWERTY] = LAYOUT(
-        KC_TAB,  KC_Q, KC_W, KC_E,  KC_R,    KC_T,                                                 KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, 
-        KC_ESC,  KC_A, KC_S, KC_D,  KC_F,    KC_G,                                                 KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, 
-        KC_LSFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B,    LT(NAV, KC_LBRC), OSL(F),   LT(NUM,KC_QUOT), LT(SHORTCUT,KC_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-                             KC_NO, KC_LGUI, KC_LCTL, KC_SPC,           KC_LALT,  KC_LEAD,         KC_SPC,               KC_BSPC, KC_RGUI, KC_NO),
+        KC_TAB,  KC_Q, KC_W, KC_E,  KC_R,    KC_T,                                                     KC_Y, KC_U, KC_I, KC_O, KC_P, KC_QUOT, 
+        KC_ESC,  KC_A, KC_S, KC_D,  KC_F,    LT(NUM,KC_G),                                             KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, 
+        KC_LSFT, KC_Z, KC_X, KC_C,  KC_V,    KC_B, LT(NAV, KC_LBRC), OSL(F),      KC_BSLS, LT(SHORTCUT,KC_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
+                             KC_NO, KC_LGUI, KC_LCTL, KC_SPC, KC_LALT,            KC_LEAD,KC_SPC, KC_BSPC, KC_RGUI, KC_NO),
     
  /*
   * Number layer
   *
   * ,-------------------------------------------.                                ,--------------------------------------------.
-  * |        |  1   |  2   |  3   |  4   |  5   |                                |  6    |   7  |   8  |   9  |   0  |        |
+  * |        |      |      |      |      |      |                                |       | 7,&  | 8,*  | 9,(  |      |        |
   * |--------+------+------+------+------+------|                                |-------+------+------+------+------+--------|
-  * |        |      |      |      |      |      |                                |       |      |      |      |      | (ENT)  |
+  * |        |      |      |      |      |(THIS)|                                |       | 4,$  | 5,%  | 6,^  |      | (ENT)  |
   * |--------+------+------+------+------+------+--------------.  ,--------------+-------+------+------+------+------+--------|
-  * | (LSFT) |      |      |      |      |      |  `~   |  -   |  |      |  =    |       |      |      |      |      | (RSFT) |
+  * | (LSFT) |      |      |      |      |      |       |      |  |  `~  |    -  |  = +  | 1,!  | 2,@  | 3,#  |      | (RSFT) |
   * `----------------------+------+------+------+-------+------|  |------+-------+-------+------+------+----------------------'
-  *                        |      |(LGUI)|(LCTL)|(Space)|(Alt) |  |(Lead)|(Space)|(BKSPC)|(RGUI)|      |
+  *                        |      |(LGUI)|(LCTL)|(Space)|(Alt) |  |(Lead)|(Space)|  0,)  |(RGUI)|      |
   *                        |      |      |      |       |      |  |      |       |       |      |      |
   *                        `-----------------------------------'  `------------------------------------'
   */
     [NUM] = LAYOUT(
-        KC_NO,   KC_1,  KC_2,  KC_3,  KC_4,    KC_5,                                            KC_6,    KC_7,    KC_8,  KC_9,  KC_0,  KC_NO, 
-        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                                           KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_TRNS, 
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_GRV,  KC_MINS,     KC_NO,   KC_EQL,  KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, KC_TRNS, 
-                               KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
+        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                                           KC_NO,  KC_7,   KC_8, KC_9, KC_0, KC_NO, 
+        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                                           KC_NO,  KC_4,   KC_5, KC_6, KC_NO, KC_TRNS, 
+        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,       KC_GRV,  KC_MINS, KC_EQL, KC_1,   KC_2, KC_3, KC_NO, KC_TRNS, 
+                               KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_0,   KC_TRNS,KC_NO),
  /*
   * F Keys layer
   *
   * ,-------------------------------------------.                                ,--------------------------------------------.
-  * |   F1   |  F2  |  F3  |  F4  |  F5  | F6   |                                |  F7   |  F8  |  F9  | F10  | F11  |  F12   |
+  * |        |      |      |      |      |      |                                |       |  F7  |  F8  |  F9  |  F12 |        |
   * |--------+------+------+------+------+------|                                |-------+------+------+------+------+--------|
-  * |  CAPS  |      |      |      |      |      |                                |       |      |      |      |      | (ENT)  |
+  * |  CAPS  |      |      |      |      |      |                                |       |  F4  |  F5  |  F6  |  F11 | (ENT)  |
   * |--------+------+------+------+------+------+--------------.  ,--------------+-------+------+------+------+------+--------|
-  * | (LSFT) |      |      |      |      |      |       |      |  |      |       |       |      |      |      |      | (RSFT) |
+  * | (LSFT) |      |      |      |      |      |       |(THIS)|  |      |       |       |  F1  |  F2  |  F3  |  F10 | (RSFT) |
   * `----------------------+------+------+------+-------+------|  |------+-------+-------+------+------+----------------------'
   *                        |      |(LGUI)|(LCTL)|(Space)|(Alt) |  |(Lead)|(Space)|(BKSPC)|(RGUI)|      |
   *                        |      |      |      |       |      |  |      |       |       |      |      |
@@ -68,9 +68,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   */    
 
     [F] = LAYOUT(
-        KC_F1,   KC_F2, KC_F3, KC_F4, KC_F5,   KC_F6,                                         KC_F7,   KC_F8,   KC_F9, KC_F10, KC_F11, KC_F12, 
-        KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                                         KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_NO,  KC_NO, 
-        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO,  KC_NO,  KC_NO,
+        KC_NO,   KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                                         KC_NO,   KC_F7,   KC_F8, KC_F9,  KC_F12, KC_NO, 
+        KC_CAPS, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,                                         KC_NO,   KC_F4,   KC_F5, KC_F6,  KC_F11, KC_NO, 
+        KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_F1,   KC_F2, KC_F3,  KC_F10, KC_NO,
                                KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
 /*
  * Navigation Layer
@@ -80,7 +80,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|                               |-------+------+------+------+------+--------|
  * |        |      |      |      |      |      |                               |       | Left | Down | Right|      |  PGDN  |
  * |--------+------+------+------+------+------+-------------.  ,--------------+-------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |      |  |      |       |       |      |      |      |      |        |
+ * |        |      |      |      |      |      |(THIS)|      |  |      |       |       |      |      |      |      |        |
  * `----------------------+------+------+------+------+------|  |------+-------+-------+------+------+----------------------'
  *                        |      |(LGUI)|(LCTL)|(Space)|(Alt)|  |(Lead)|(Space)|(BKSPC)|(RGUI)|      |
  *                        |      |      |      |      |      |  |      |       |       |      |      |
@@ -93,24 +93,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                              KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
     
 /*
- * Navigation Layer
+ * Shortcut Layer
  *
- * ,---------------------------------------------.                               ,--------------------------------------------.
- * |LSFT(F6)|LALT(F7)|      |      |      |      |                               |       |      |      |      |      |        |
- * |--------+--------+------+------+------+------|                               |-------+------+------+------+------+--------|
- * |        |        |      |      |      |      |                               |       |      |      |      |      |        |
- * |--------+--------+------+------+------+------+-------------.  ,--------------+-------+------+------+------+------+--------|
- * |        |        |      |      |      |      |      |      |  |      |       |       |      |      |      |      |        |
- * `------------------------+------+------+------+------+------|  |------+-------+-------+------+------+----------------------'
- *                          |      |(LGUI)|(LCTL)|(Space)|(Alt)|  |(Lead)|(Space)|(BKSPC)|(RGUI)|      |
- *                          |      |      |      |      |      |  |      |       |       |      |      |
- *                          `----------------------------------'  `------------------------------------'
+ * ,-------------------------------------------------.                               ,--------------------------------------------.
+ * |        | LGUI(1)|LGUI(2)|LGUI(3)|LGUI(4)|LGUI(5)|                               |       |      |      |      |      |        |
+ * |--------+--------+-------+-------+-------+-------|                               |-------+------+------+------+------+--------|
+ * |LSFT(F6)|LALT(F7)|       |       |       |       |                               |       |      |      |      |      |        |
+ * |--------+--------+-------+-------+-------+-------+-------------.  ,--------------+-------+------+------+------+------+--------|
+ * |        |        |       |       |       |       |      |      |  |      | (THIS)|       |      |      |      |      |        |
+ * `-------------------------+-------+-------+-------+------+------|  |------+-------+-------+------+------+----------------------'
+ *                           |       |(LGUI) |(LCTL) |(Space)|(Alt)|  |(Lead)|(Space)|(BKSPC)|(RGUI)|      |
+ *                           |       |       |       |      |      |  |      |       |       |      |      |
+ *                           `-------------------------------------'  `------------------------------------'
  */     
     [SHORTCUT] = LAYOUT(
-        LSFT(KC_F6), LALT(KC_F7), KC_NO, KC_NO, KC_NO,   KC_NO,                                          KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
-        KC_NO,       KC_NO,       KC_NO, KC_NO, KC_NO,   KC_NO,                                          KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
-        KC_NO,       KC_NO,       KC_NO, KC_NO, KC_NO,   KC_NO,   KC_NO,   KC_NO,      KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
-                                         KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO),
+        KC_NO,       LGUI(KC_1),  LGUI(KC_2), LGUI(KC_3), LGUI(KC_4),   LGUI(KC_5),                                          KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
+        LSFT(KC_F6), LALT(KC_F7), KC_NO,      KC_NO,      KC_NO,        KC_NO,                                               KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
+        KC_NO,       KC_NO,       KC_NO,      KC_NO,      KC_NO,        KC_NO,    KC_NO,   KC_NO,      KC_NO,   KC_NO,       KC_NO,   KC_NO,   KC_NO,   KC_NO, KC_NO, KC_NO, 
+                                              KC_NO,      KC_TRNS,      KC_TRNS,  KC_TRNS, KC_TRNS,    KC_TRNS, KC_TRNS,     KC_TRNS, KC_TRNS, KC_NO),
 };
 
 
@@ -145,7 +145,7 @@ static void render_qmk_logo(void) {
 static void render_status(void) {
     // QMK Logo and version information
     render_qmk_logo();
-    oled_write_P(PSTR("       nchris v0.1.4\n\n"), false);
+    oled_write_P(PSTR("       nchris v0.1.5\n\n"), false);
 
     // Host Keyboard Layer Status
     oled_write_P(PSTR("Layer: "), false);
